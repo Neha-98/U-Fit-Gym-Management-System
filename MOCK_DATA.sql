775f17701f4133bff89741bf5f8215a56cc3f680
Neha-98 <52178256+Neha-98@users.noreply.github.com>
2025-08-26 01:15:33 -0400

    Add files via upload

---
+++ MOCK_DATA.sql
@@ -0,0 +1,22 @@
+create table MOCK_DATA (
+	member_id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	age INT,
+	email VARCHAR(50),
+	country VARCHAR(50),
+	postal_code VARCHAR(50),
+	membership_start_date DATE,
+	membership_end_date DATE,
+	membership_type VARCHAR(7)
+);
+insert into MOCK_DATA (member_id, first_name, last_name, age, email, country, postal_code, membership_start_date, membership_end_date, membership_type) values (1, 'Audy', 'Stoodley', 41, 'astoodley0@cocolog-nifty.com', 'Philippines', '6016', '7/17/2022', '3/1/2022', 'VIP');
+insert into MOCK_DATA (member_id, first_name, last_name, age, email, country, postal_code, membership_start_date, membership_end_date, membership_type) values (2, 'Petronella', 'Grevatt', 61, 'pgrevatt1@google.com.au', 'Japan', '989-1505', '12/22/2022', '12/30/2022', 'Premium');
+insert into MOCK_DATA (member_id, first_name, last_name, age, email, country, postal_code, membership_start_date, membership_end_date, membership_type) values (3, 'Kaycee', 'Bassill', 54, 'kbassill2@umn.edu', 'Indonesia', null, '5/7/2022', '7/7/2022', 'Basic');
+insert into MOCK_DATA (member_id, first_name, last_name, age, email, country, postal_code, membership_start_date, membership_end_date, membership_type) values (4, 'Marshall', 'Kubiak', 51, 'mkubiak3@wufoo.com', 'China', null, '4/13/2022', '5/21/2022', 'Basic');
+insert into MOCK_DATA (member_id, first_name, last_name, age, email, country, postal_code, membership_start_date, membership_end_date, membership_type) values (5, 'Christy', 'Cawthron', 64, 'ccawthron4@virginia.edu', 'Canada', 'J8Y', '2/8/2022', '5/12/2022', 'Basic');
+insert into MOCK_DATA (member_id, first_name, last_name, age, email, country, postal_code, membership_start_date, membership_end_date, membership_type) values (6, 'Cicely', 'Lamlin', 36, 'clamlin5@amazon.co.uk', 'United States', '25331', '3/15/2022', '2/9/2022', 'Basic');
+insert into MOCK_DATA (member_id, first_name, last_name, age, email, country, postal_code, membership_start_date, membership_end_date, membership_type) values (7, 'Joly', 'Baroche', 36, 'jbaroche6@moonfruit.com', 'China', null, '2/26/2022', '6/13/2022', 'VIP');
+insert into MOCK_DATA (member_id, first_name, last_name, age, email, country, postal_code, membership_start_date, membership_end_date, membership_type) values (8, 'Tuck', 'Gawthrope', 29, 'tgawthrope7@time.com', 'Czech Republic', '742 65', '5/7/2022', '6/14/2022', 'Basic');
+insert into MOCK_DATA (member_id, first_name, last_name, age, email, country, postal_code, membership_start_date, membership_end_date, membership_type) values (9, 'Ange', 'Toe', 18, 'atoe8@deliciousdays.com', 'Cyprus', null, '6/6/2022', '1/5/2022', 'Basic');
+insert into MOCK_DATA (member_id, first_name, last_name, age, email, country, postal_code, membership_start_date, membership_end_date, membership_type) values (10, 'Marlowe', 'Shilling', 69, 'mshilling9@jiathis.com', 'Philippines', '4123', '3/19/2022', '8/27/2022', 'Premium');
